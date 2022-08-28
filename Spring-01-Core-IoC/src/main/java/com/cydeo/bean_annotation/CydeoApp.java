817857e8b6c3ff33185af7125b3ac1b98b5f98e8
anaburea <lien82@gmail.com>
2022-08-28 12:59:11 -0400

    Bean Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,26 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+        ft.createAccount();
+
+        PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+        pt.createAccount();
+
+        String str = container.getBean(String.class);
+        System.out.println(str);
+
+        Integer number = container.getBean(Integer.class);
+        System.out.println(number);
+
+    }
+
+}
