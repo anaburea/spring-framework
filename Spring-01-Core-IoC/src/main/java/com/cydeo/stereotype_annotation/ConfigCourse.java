f4384a78c7787b997340ba80a19ae64716feabfb
anaburea <lien82@gmail.com>
2022-08-31 22:28:50 -0400

    Stereotype Annotations

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -0,0 +1,11 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan
+public class ConfigCourse {
+
+
+}
