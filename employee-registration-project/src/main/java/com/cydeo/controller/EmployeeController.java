ae095e0b8cbfb80e24442d661f119ec97c7394da
anaburea <lien82@gmail.com>
2022-10-02 18:43:57 -0400

    Employee registration project

--- employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
+++ employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -4,8 +4,13 @@ import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -18,6 +23,23 @@ public class EmployeeController {
         model.addAttribute("states", DataGenerator.getAllStates());
 
         return "employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("states", DataGenerator.getAllStates());
+
+            return "employee/employee-create";
+        }
+
+        DataGenerator.saveEmployee(employee);
+
+        model.addAttribute("employees", DataGenerator.readAllEmployees());
+
+        return "employee/employee-list";
     }
 
 }
