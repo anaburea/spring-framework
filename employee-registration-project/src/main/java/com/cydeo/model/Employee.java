c05770f34fd88b5707fe1983acfa55f9b63d72a3
anaburea <lien82@gmail.com>
2022-10-02 15:41:28 -0400

    Employee registration project

---
+++ employee-registration-project/src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,22 @@
+package com.cydeo.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String email;
+    private String password;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+}
